82c30c16edb92040da40d5e991de2d872407c84a
bala-amypo <bala@amypo.in>
2025-12-17 10:51:15 +0000

    User commit

--- src/main/java/com/controller/Studentcontroller.java
+++ src/main/java/com/controller/Studentcontroller.java
@@ -2,7 +2,7 @@ package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.demo.entity.studentEntity;
+import com.example.demo.entity.StudentEntity;
 
 import com.example.demo.service.StudentService;
 
--- src/main/java/com/service/StudentService.java
+++ src/main/java/com/service/StudentService.java
@@ -2,7 +2,7 @@ package com.example.demo.service;
 
 import com.example.demo.entity.StudentEntity;
 
-public interface StudentService() {
+public interface StudentService{
   StudentEntity postData(StudentEntity stu);
 
 
