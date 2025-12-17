9138b1646cb90bad0140b21ddc9aa1005ec8f02a
bala-amypo <bala@amypo.in>
2025-12-17 09:06:15 +0000

    User commit

--- src/main/java/com/service/Impl/StudentServiceImpl.java
+++ src/main/java/com/service/Impl/StudentServiceImpl.java
@@ -2,10 +2,12 @@ package com.example.demo.service.Impl;
 
 import org.springframework.stereotype.service;
 
-import com.example.demo
+import com.example.demo.service.StudentService;
 
 @service
 
 public class StudentServiceImpl implements StudentService{
+
+   public StudentEntity postData(StudentEntity stu);
 
 }
