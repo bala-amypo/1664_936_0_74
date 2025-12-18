7b1982be3f1929e64abc48e07f4ec56a438329df
bala-amypo <bala@amypo.in>
2025-12-18 04:10:41 +0000

    User commit

--- src/main/java/com/config/SwaggerConfig.java
+++ src/main/java/com/config/SwaggerConfig.java
@@ -14,7 +14,7 @@ public class SwaggerConfig {
         return new OpenAPI()
                 
                 .servers(List.of(
-                        new Server().url("https://9045.408procr.amypo.ai/")
+                        new Server().url("https://9094.408procr.amypo.ai/")
                 ));
         }
 }
--- src/main/java/com/controller/Studentcontroller.java
+++ src/main/java/com/controller/Studentcontroller.java
@@ -3,6 +3,7 @@ package com.example.demo.controller;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.postMapping;
 import com.example.demo.entity.StudentEntity;
+import org.spring.
 
 import com.example.demo.service.StudentService;
 
