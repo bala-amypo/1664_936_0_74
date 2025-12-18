1392b6bd530655121e5f21075f08c4b7ee918306
bala-amypo <bala@amypo.in>
2025-12-18 08:05:41 +0000

    User commit

--- src/main/java/com/example/demo/entity/ValidationEntity.java
+++ src/main/java/com/example/demo/entity/ValidationEntity.java
@@ -1,16 +1,20 @@
 package com.example.demo.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import jakarta.pers
+import jakarta.persistence.GeneratedValue;
+import jakarata.persistence.GenerationType;
+import jakarata.validation.constraints.NotNull;
+import jakarata,.validation.constraints.Size;
 
 @Entity
 public class ValidationEntity{
     @id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-
-
-    private Long id;
+ private Long id;
+ @NotNull
+ @size(min = 2, max = 0, message = "must be to 10 character")
     private String username;
+    @Email
     private String email;
     private String password;
     private int age;
