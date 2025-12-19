04f405d476325e91a9d074aa28f88d8c531bd47d
bala-amypo <bala@amypo.in>
2025-12-19 06:28:27 +0000

    User commit

--- src/main/java/com/example/demo/entity/StudentEntity.java
+++ src/main/java/com/example/demo/entity/StudentEntity.java
@@ -26,21 +26,20 @@ public String getName(){
 public void setName(String name){
     this.name=name;
 }
-// Id
+
 public Integer getId(){
     return id;
 }
 public void setId(Integer id){
     this.id=id;
 }
-// email
+
 public String getEmail(){
     return email;
 }
 public void setEmail(String email){
     this.email=email;
 }
-// password
 public String getPassword(){
     return password;
 }
--- src/main/java/com/example/demo/entity/ValidationEntity.java
+++ src/main/java/com/example/demo/entity/ValidationEntity.java
@@ -29,61 +29,7 @@ public class ValidationEntity{
     @Max(30)
     @Positive(message="age could not be negative")
     private int age;
-      
-    public String getUsername(){
-        return username;
-    }
-    public void setUsername(){
-        this.username=username;
-    }
+          
 
-    public Long getId(){
-        return id;
-    }
-    public void setId(Long id){
-        this.id=id;
-    }
-
-    public String getEmail(){
-        return email;
-    }
-    public void setEmail(String email){
-        this.email=email;
-    }
-
-    public String getPassword(){
-        return password;
-    }
-    public void setPassword(String password){
-        this.password=password;
-    }
-
-    public int getAge(){
-        return age;
-    }
-    public void setAge(int age){
-        this.age=age;
-    }
-
-     
-    public ValidationEntity(  Long id,
-    @NotNull
-    @Size(min=2,max=6,message="require 2 to 6 characters")
-     String username,
-    @Email(message="Email is not valid")
-     String email,
-    @Size(min=2,max=6,message="should be less than 6")
-    @NotNull(message="password is mandatory")     String password,
-    @Max(30)
-    @Positive(message="age could not be negative")
-     int age){
-       this.id=id;
-        this.username=username;
-        this.email=email;
-        this.password=password;
-        this.age=age;
-
-        
-    }
      
 }
