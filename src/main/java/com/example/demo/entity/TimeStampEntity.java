b1e7c8058c149a669f6702bc81f21a910d7b8a49
bala-amypo <bala@amypo.in>
2025-12-19 07:58:27 +0000

    User commit

--- src/main/java/com/example/demo/entity/TimeStampEntity.java
+++ src/main/java/com/example/demo/entity/TimeStampEntity.java
@@ -4,4 +4,12 @@ package com.example.demo.entity;
 
 @Entity
 @Data 
-@
+@AllArgsConstructor
+public class TimeStampEntity{
+    private Long id;
+    private String username;
+    private String email;
+    private String password;
+    private Date created;
+
+}
