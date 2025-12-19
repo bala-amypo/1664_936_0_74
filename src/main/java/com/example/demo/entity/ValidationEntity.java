e46db9f0a3077c279572d502d75bd7e0328a45ba
bala-amypo <bala@amypo.in>
2025-12-19 06:31:27 +0000

    User commit

--- src/main/java/com/example/demo/entity/ValidationEntity.java
+++ src/main/java/com/example/demo/entity/ValidationEntity.java
@@ -9,11 +9,11 @@ import jakarta.validation.constraints.Size;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Positive;
-
+import lombok.Data;
 
 @Entity
-
-
+@Data
+@AllArgsCons
 public class ValidationEntity{
 @Id
 @GeneratedValue(strategy=GenerationType.IDENTITY)
