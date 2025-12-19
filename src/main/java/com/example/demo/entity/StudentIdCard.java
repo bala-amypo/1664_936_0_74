80a3e1ca3092d53362b8b5c5f35ca2cb0295237b
bala-amypo <bala@amypo.in>
2025-12-19 10:04:27 +0000

    User commit

--- src/main/java/com/example/demo/entity/StudentIdCard.java
+++ src/main/java/com/example/demo/entity/StudentIdCard.java
@@ -5,29 +5,24 @@ import jakarta.persistence.*;
 import jakarta.persistence.Id;
 import java.util.*;
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 import lombok.AllArgsConstructor;
 import java.time.LocalDateTime;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import lombok.NoAgrsConstructor;
-import jakarta.persistence.PrePersist;
-import jakarta.persistence.PreUpdate;
-
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.JoinCloumn;
 @Entity
 @Data
 @AllAgrsConstructor
 @NoAgrsConstructor
 public class StudentIdCard{
     @Id
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
-    private Long id;
-    private String name;
-    private String email;
-    private LocalDateTime createdAt;
-    private LocalDateTime updatedAt;
+    @Gene
 
-   
+@OneToOne
+@JoinColumn(name = "student_id")
+private Studentdetails student;
+
 
 }
